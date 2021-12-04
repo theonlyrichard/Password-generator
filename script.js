f19132a95d29c0c16f8d33ddc3b523c27ae28f96
Richard Cardoza <93751925+theonlyrichard@users.noreply.github.com>
2021-12-04 14:48:33 -0800

    Add files via upload

---
+++ script.js
@@ -0,0 +1,71 @@
+// Assignment code here
+
+function generatePassword() {
+  var lowerCaseSet = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
+  var upperCaseSet = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
+  var numSet = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"];
+  var specialSet = ["@", "$", "#", "!", "%", "^", "&", "*", "(", ")", "+", "?", "/", "-", ":", ";", "[", "]", "{", "}", ".", "<", ">", "=", "_", "`", "|", "~"];
+  var selectedArray = [];
+
+  var passwordLength = getPasswordLength();
+
+  var charTypeSelected = false;
+  // This loop ensures the user selects at least one character type
+  while (charTypeSelected == false) {
+    var lowerCase = getChoice("lowercase");
+    var upperCase = getChoice("uppercase");
+    var numericCharacters = getChoice("numeric");
+    var specialCharacters = getChoice("special");
+    if ((lowerCase) || (upperCase) || (numericCharacters) || (specialCharacters)) {
+      charTypeSelected = true;
+    } else {
+
+    }
+  }
+
+
+  if (lowerCase) {
+    selectedArray = selectedArray.concat(lowerCaseSet);
+  }
+  if (upperCase) {
+    selectedArray = selectedArray.concat(upperCaseSet);
+  }
+  if (numericCharacters) {
+    selectedArray = selectedArray.concat(numSet);
+  }
+  if (specialCharacters) {
+    selectedArray = selectedArray.concat(specialSet);
+  }
+
+  var passwordString = "";
+  // This loop will take the appended array, randomly select elements from it, then append the selections to a string, generating the password.
+  for (var i = 0; i < passwordLength; i++) {
+    passwordString += selectedArray[Math.floor(Math.random() * (selectedArray.length))];
+  }
+
+  return passwordString;
+}
+
+function getPasswordLength() {
+  var userChoice = 0;
+  while ((userChoice < 8) || (userChoice > 128)) {
+    userChoice = parseInt(window.prompt("Enter the number of characters between 8 and 128: "));
+  }
+
+  return userChoice;
+}
+
+
+// Get references to the #generate element
+var generateBtn = document.querySelector("#generate");
+
+// Write password to the #password input
+function writePassword() {
+  var password = generatePassword();
+  var passwordText = document.querySelector("#password");
+
+  passwordText.value = password;
+}
+
+// Add event listener to generate button
+generateBtn.addEventListener("click", writePassword);
